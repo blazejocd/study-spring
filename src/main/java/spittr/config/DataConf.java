48a4e4ef1083cdfa4017c578df443db3d4b1451f
Blazej <blazejocd@op.pl>
2017-02-12 18:44:45 +0100

    JDBC NamedParameterJdbcTemplate working

--- src/main/java/spittr/config/DataConf.java
+++ src/main/java/spittr/config/DataConf.java
@@ -4,7 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
@@ -22,8 +22,8 @@ public class DataConf
 	}
 	
 	@Bean
-	public JdbcTemplate jdbcTemplate(DataSource dataSource)
+	public NamedParameterJdbcTemplate jdbcTemplate(DataSource dataSource)
 	{
-		return new JdbcTemplate(dataSource);
+		return new NamedParameterJdbcTemplate(dataSource);
 	}
 }
--- src/main/java/spittr/data/Spitters.java
+++ src/main/java/spittr/data/Spitters.java
@@ -1,11 +1,14 @@
 package spittr.data;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -14,21 +17,71 @@ import spittr.Spitter;
 @Component
 public class Spitters implements SpitterRepository 
 {	
-	private JdbcOperations jdbcOperations;
+	private NamedParameterJdbcTemplate jdbcOperations;
+	
+	private Map<String, Object> params;
 	
 	private static final String SEARCH_BY_USERNAME_SQL =
-			"SELECT * FROM users WHERE username = ?";
+			"SELECT * FROM users WHERE username = :username";
 	
 	private static final String INSERT_SQL = 
-			"INSERT INTO users (username, password, firstName, lastName, email) VALUES (?,?,?,?,?)";
+			"INSERT INTO users (username, password, firstName, lastName, email) "
+			+"VALUES (:username,:password,:firstName,:lastName,:email)";
 	
 	private static final String UPDATE_SQL = 
-			"UPDATE users SET username = ?, password = ?, firstName = ?, lastName = ?, email = ? WHERE id = ?";
+			"UPDATE users SET username = :username, password = :password, "
+			+"firstName = :firstName, lastName = :lastName, email = :email WHERE id = :id";
+	
+	private static final String PRIVATE_ENCODE_KEY = "1234";
 
 	@Autowired
-	public Spitters(JdbcOperations jdbcOps)
+	public Spitters(NamedParameterJdbcOperations jdbcOps)
 	{
-		this.jdbcOperations = jdbcOps;
+		this.jdbcOperations = (NamedParameterJdbcTemplate) jdbcOps;
+		this.params = new HashMap<>();
+	}
+
+	@Override
+	public Spitter findByUsername(String username)
+	{
+		this.params.clear();
+		this.params.put("username", username);
+		
+		try {
+			Spitter spitter = this.jdbcOperations.queryForObject(
+					SEARCH_BY_USERNAME_SQL,
+					this.params,
+					new SpitterRowMapper());
+			return spitter;
+		} catch (DataAccessException e) 
+		{
+			return null;
+		}
+	}
+	
+	public int addSpitter(Spitter spitter)
+	{
+		params.clear();
+		params.put("username", spitter.getUsername());
+		params.put("password", this.encodePassword(spitter.getPassword()));
+		params.put("firstName", spitter.getFirstName());
+		params.put("lastName", spitter.getLastName());
+		params.put("email", spitter.getEmail());
+		
+		return this.jdbcOperations.update(INSERT_SQL, params);
+	}
+	
+	public int updateSpitter(Spitter spitter, Long id)
+	{
+		params.clear();
+		params.put("id", id);
+		params.put("username", spitter.getUsername());
+		params.put("password", this.encodePassword(spitter.getPassword()));
+		params.put("firstName", spitter.getFirstName());
+		params.put("lastName", spitter.getLastName());
+		params.put("email", spitter.getEmail());
+		
+		return this.jdbcOperations.update(UPDATE_SQL, params);
 	}
 	
 	@Override
@@ -37,26 +90,11 @@ public class Spitters implements SpitterRepository
 		Spitter existingSpitter = this.findByUsername(spitter.getUsername());
 		if (null == existingSpitter)
 		{
-			this.jdbcOperations.update(
-				INSERT_SQL,
-				spitter.getUsername(),
-				new StandardPasswordEncoder("1234")
-					.encode(spitter.getPassword()),
-				spitter.getFirstName(),
-				spitter.getLastName(),
-				spitter.getEmail());
+			this.addSpitter(spitter);
 		}
 		else
 		{
-			this.jdbcOperations.update(
-				UPDATE_SQL,
-				spitter.getUsername(),
-				new StandardPasswordEncoder("1234")
-					.encode(spitter.getPassword()),
-				spitter.getFirstName(),
-				spitter.getLastName(),
-				spitter.getEmail(),
-				existingSpitter.getId());
+			this.updateSpitter(spitter, existingSpitter.getId());
 		}
 			
 		Spitter newSpitter = this.findByUsername(spitter.getUsername());
@@ -65,20 +103,11 @@ public class Spitters implements SpitterRepository
 		}
 		return null;
 	}
-
-	@Override
-	public Spitter findByUsername(String username)
+	
+	private String encodePassword(String password)
 	{
-		try {
-			Spitter spitter = this.jdbcOperations.queryForObject(
-					SEARCH_BY_USERNAME_SQL, 
-					new SpitterRowMapper(), 
-					username);
-			return spitter;
-		} catch (DataAccessException e) 
-		{
-			return null;
-		}
+		return new StandardPasswordEncoder(PRIVATE_ENCODE_KEY)
+				.encode(password);
 	}
 }
 
